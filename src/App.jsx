a699d96dba926eca97ec807ee6e55d4681495288
Nikimad <PybePoiD@gmail.com>
2023-06-22 13:09:46 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,28 @@
-import { useState } from "react";
-
-const App = () => {
-  const [val, setVal] = useState("");
-
-  return (
-    <div>
-      <form>
-        <label>
-          Title
-          <input
-            type="text"
-            value={val}
-            onChange={(e) => setVal(e.target.value)}
-          />
-        </label>
-        <label>
-          <input type="submit" />
-        </label>
-      </form>
-      <button
-        onClick={() => {
-          fetch("api/boards", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              title: val,
-            }),
-          })
-          .catch((e) => console.log(e));
-        }}
-      >
-        POST
-      </button>
-      <button onClick={() => fetch("api/boards").then(res => res.json()).then(data => console.log(data))}>Get</button>
-    </div>
-  );
-};
+const App = () => (
+  <>
+    <button
+      onClick={() =>
+        fetch("api/boards")
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+      }
+    >
+      Get
+    </button>
+    <button
+      onClick={() =>
+        fetch("api/boards", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ createdAt: Date.now() }),
+        })
+      }
+    >
+      Post
+    </button>
+  </>
+);
 
 export default App;
